f43a37a9b974f10603f92f49b3e8333521739def
Henrique Espindola <kamus@hadenes.io>
2023-04-06 11:42:51 -0300

    🚀 chore(get_version.sh): move get_version.sh to .github/scripts/ 🚀 chore(release-notes.py): move release-notes.py to .github/scripts/ 🚀 chore(requirements.txt): move requirements.txt to .github/scripts/ 🚀 chore(goreleaser.yml): add SKIP_CHANGELOG environment variable 🚀 chore(release-notes.yml): add workflow to improve release notes 🚀 chore(goreleaser/*.yaml): skip changelog if SKIP_CHANGELOG is true 🚀 chore(flags.go): move get_version.sh to .github/scripts/

---
+++ .github/scripts/release-notes.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import os
+from functools import reduce
+from typing import AnyStr, Tuple, Dict
+
+import openai
+import requests
+
+
+def get_release(release: AnyStr = "latest") -> Dict:
+    """Get the release message from the GitHub API."""
+
+    print('[*] Getting release message...')
+
+    return requests.get(
+        "https://api.github.com/repos/kamushadenes/chloe/releases/{}".format(release),
+        headers={
+            "Accept": "application/vnd.github+json",
+            "Authorization": "Bearer {}".format(os.environ["GITHUB_TOKEN"])
+        },
+    ).json()
+
+
+def get_improved_release_message(release: Dict) -> Tuple[Dict, AnyStr]:
+    """Improves the release message using OpenAI's GPT-3 API."""
+
+    print('[*] Improving release message...')
+
+    completion = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=[
+        {
+            "role": "system",
+            "content": "Your task is to rewrite release notes in a more concise manner, "
+                       "no need to mention specific commits. "
+                       "Group things by features / bug fixes / etc as appropriate. "
+                       "Try to focus on the most important changes. "
+                       "Return it in markdown format.",
+        },
+        {
+            "role": "user",
+            "content": release['body']
+        }
+    ])
+
+    return release, completion.choices[0].message.content
+
+
+def update_release_notes(args: Tuple[Dict, AnyStr]) -> None:
+    """Update the release notes using the GitHub API."""
+
+    print('[*] Updating release notes...')
+
+    print('[*] New content:\n\n{}\n\n'.format(args[1]))
+
+    r = requests.patch(
+        "https://api.github.com/repos/kamushadenes/chloe/releases/{}".format(args[0]['id']),
+        headers={
+            "Accept": "application/vnd.github+json",
+            "Authorization": "Bearer {}".format(os.environ["GITHUB_TOKEN"])
+        },
+        json={
+            "body": args[1],
+        }
+    )
+
+    if r.status_code != 200:
+        print("[-] Failed to update release notes: {}".format(r.text))
+    else:
+        print('[+] Successfully updated release notes!')
+
+
+if __name__ == '__main__':
+    if "GITHUB_TOKEN" not in os.environ:
+        raise ValueError("GITHUB_TOKEN environment variable is not set.")
+
+    if "OPENAI_API_KEY" not in os.environ:
+        raise ValueError("OPENAI_API_KEY environment variable is not set.")
+
+    reduce(lambda x, f: f(x),
+           [
+               get_release,
+               get_improved_release_message,
+               update_release_notes,
+           ], "latest")
--- flags/flags.go
+++ flags/flags.go
@@ -6,6 +6,6 @@ import (
 
 var InteractiveCLI bool
 
-//go:generate bash ../scripts/get_version.sh
+//go:generate bash ../.github/scripts/get_version.sh
 //go:embed version.txt
 var Version string
